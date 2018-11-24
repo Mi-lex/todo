774a1333ddce8a2d1fc90704b99d28deea1957ca
Mi-lex <mishin-lex@mail.ru>
2018-11-24 13:09:59 +0300

    Make options appear on long click

--- src/js/components/List/Item/Item.js
+++ src/js/components/List/Item/Item.js
@@ -9,15 +9,25 @@ class Item extends PureComponent {
         super(props);
 
         this.state = {
-            showOptions: false,
+            /**
+             * When new item is created
+             * it has no value therefore
+             * it's editable by default
+             */
             isChanging: this.props.text.length === 0,
+            showOptions: false,
             completed: false
         }
 
         this.text = React.createRef();
-        this.removeSelf = this.removeSelf.bind(this);
+        this.edit = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
         this.saveChanges = this.saveChanges.bind(this);
+        this.openOptions = this.openOptions.bind(this);
         this.completeTask = this.completeTask.bind(this);
+        this.toggleOptions = this.toggleOptions.bind(this);
+        this.shouldGetEditable = this.shouldGetEditable.bind(this);
+        this.cancelOpenOptions = this.cancelOpenOptions.bind(this);
     }
 
     get options() {
@@ -31,12 +41,13 @@ class Item extends PureComponent {
                 <Button
                     className={editClassname}
                     purpose='Edit Task'
-                    iconName='edit'/>
+                    iconName='edit'
+                    click={this.edit}/>
                 <Button
                     className={deleteClassName} 
                     purpose='Delete task'
                     iconName='delete'
-                    onClick={this.removeSelf}/>
+                    click={this.remove}/>
             </div>
         )
     }
@@ -48,10 +59,7 @@ class Item extends PureComponent {
         const contentClassName = styles['content'];
 
         let content;
-        /**
-         * Use textarea for content depending on whether item is
-         * getting edited or not
-         */
+        // Use <textarea> instead of <p> whe editing is needed
         if (this.state.isChanging) {
             content = 
                 <Aux>
@@ -79,14 +87,22 @@ class Item extends PureComponent {
         return content;
     }
 
-    removeSelf() {
+    remove() {
         this.props.removeItem(this.props.itemKey);
     }
 
-    toggleChangingMod() {
+    edit() {
         this.setState({
             ...this.state,
-            isChanging: !this.state.isChanging
+            showOptions: false,
+            isChanging: true
+        })
+    }
+
+    shouldGetEditable(boolean) {
+        this.setState({
+            ...this.state,
+            isChanging: boolean
         })
     }
 
@@ -94,10 +110,10 @@ class Item extends PureComponent {
         const text = e.target.value;
 
         if (text.length === 0) {
-            this.removeSelf();
+            this.remove();
         } else {
             this.props.saveChanges(this.props.itemKey, text);
-            this.toggleChangingMod();
+            this.shouldGetEditable(false);
         }
     }
 
@@ -108,13 +124,36 @@ class Item extends PureComponent {
         })
     }
 
+    toggleOptions() {
+        this.setState({
+            ...this.state,
+            showOptions: !this.state.showOptions
+        })
+    }
+
+    openOptions() {
+        if (!this.state.isChanging) {
+            this.__optionsTimer = setTimeout(() => {
+                this.toggleOptions(true);
+            }, 300);
+        }
+    }
+
+    cancelOpenOptions() {
+        if (this.__optionsTimer) {
+            clearTimeout(this.__optionsTimer);
+        }
+    }
+
     render() {
         const itemClassNames = this.state.completed ?
             styleExtractor(styles, ['item', 'item--completed']) :
             styles['item'];
 
         return (
-            <li className={itemClassNames}>
+            <li className={itemClassNames}
+                onMouseDown={this.openOptions}
+                onMouseUp={this.cancelOpenOptions}>
                 {this.content}
             </li>
         )
